ddf951b4e133eb53e31fbf33566758cd41e94ede
rishig1998 <rishig1998@gmail.com>
2020-12-06 21:10:08 +0530

    lession-3

--- app/src/main/java/com/example/kennygame/MainActivity.kt
+++ app/src/main/java/com/example/kennygame/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.kennygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var score=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        score=0
+    }
+    fun increasescore(view:View)
+    {
+        score++
+        scoreText.text="Score:"+score
     }
 }
